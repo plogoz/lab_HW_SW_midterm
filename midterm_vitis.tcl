c37c1f1a2a639d418cdff7e8b9f9f62e8cbaec4b
plogoz <162329871+plogoz@users.noreply.github.com>
2026-03-13 15:16:24 +0100

    Modify midterm_vitis.tcl for simulation control

--- midterm_vitis.tcl
+++ midterm_vitis.tcl
@@ -3,17 +3,26 @@
 ## Please DO NOT edit it.
 ## Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ############################################################
+set RUN_CSIM 0
+if { [llength $argv] > 0 } {
+    set RUN_CSIM [lindex $argv 0]
+}
+
 open_project midterm_vitis
 set_top Conv2D_HW
 add_files HLS/conv2d.cpp
 add_files HLS/conv2d.h
 add_files -tb HLS/conv2DTestbench.cpp
-open_solution "solution1" -flow_target vivado
-set_part {xc7z020clg400-1}
+
+open_solution solution1 -flow_target vivado
+set_part xc7z020clg400-1
 create_clock -period 10 -name default
 config_export -output ./IP
-#source "./midterm_vitis/solution1/directives.tcl"
-csim_design -clean
+
+if { $RUN_CSIM } {
+    csim_design -clean
+}
+
 csynth_design
-cosim_design
 export_design -rtl verilog -format ip_catalog -output ./IP
+exit
